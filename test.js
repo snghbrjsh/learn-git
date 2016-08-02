27e5b021f5c587ce399242294ee7a596605217b1
noname <snghbrjsh@gmail.com>
2016-08-03 00:09:23 +0530

    qwerty

--- test.js
+++ test.js
@@ -26,6 +26,15 @@ function helpers_voc_get_response(vocResponses, uid, question, answer, value, se
                obj[question.get('id')].questionAnswerMappingId = answer.get('questionAnswerMappingId');
            }
 
+B
+B
+B
+B
+B
+C
+C
+C
+		d
            vocResponses.push(obj);
            return vocResponses;
        },
